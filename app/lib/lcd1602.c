e78590fe3f122ef0b8ea3047cf7fa1e4369d43e6
veldigfastpack <michael.a.strangewood@gmail.com>
2026-01-31 20:26:42 +0300

    something what looks like lcd support is here

---
+++ app/lib/lcd1602.c
@@ -0,0 +1,56 @@
+#include <main.h>
+#include "lcd1602.h"
+
+extern UART_HandleTypeDef huart2;
+extern I2C_HandleTypeDef i2c;
+
+void lcd1602_init(void) {	
+  HAL_Delay(50);
+  lcd1602_transmit_command(0x33);
+  HAL_Delay(5);
+  lcd1602_transmit_command(0x32);
+  HAL_Delay(1);
+  lcd1602_transmit_command(0x28);
+  HAL_Delay(1);
+  lcd1602_transmit_command(0x08);
+  HAL_Delay(1);
+  lcd1602_transmit_command(0x01);
+  HAL_Delay(2);
+  lcd1602_transmit_command(0x06);
+  HAL_Delay(1);
+  lcd1602_transmit_command(0x0C);
+}
+
+void lcd1602_send_string(char *str) {
+  if (sizeof(*str) == 0) {
+    HAL_UART_Transmit(&huart2, (const uint8_t*)"Warning: in the send function 'lcd1602_send_string' you pass an empty string\n", \
+    strlen((char *)"Warning: in the send function 'lcd1602_send_string' you pass an empty string\n"), HAL_MAX_DELAY);
+  }
+  while(*str) {
+    lcd1602_transmit_data((uint8_t)(*str));
+    str++;
+  }
+}
+
+HAL_StatusTypeDef lcd1602_transmit(uint8_t data, uint8_t flags) {
+	uint8_t upper_bits = data & 0xF0;
+	uint8_t lower_bits = (data << 4) & 0xF0;
+	uint8_t data_arr[4];                              
+	data_arr[0] = upper_bits | flags | BACKLIGHT | PIN_EN; 
+	data_arr[1] = upper_bits | flags | BACKLIGHT;         
+	data_arr[2] = lower_bits | flags | BACKLIGHT | PIN_EN;
+	data_arr[3] = lower_bits | flags | BACKLIGHT;
+	if(HAL_I2C_Master_Transmit(&i2c, lcd1604_addr, data_arr, sizeof(data_arr), HAL_MAX_DELAY) != HAL_OK) {
+        return HAL_ERROR;
+  } else {
+        return HAL_OK;
+  }
+}
+
+void lcd1602_transmit_data(uint8_t data) {
+	lcd1602_transmit(data, PIN_RS);
+}
+
+void lcd1602_transmit_command(uint8_t cmd) {
+	lcd1602_transmit(cmd, 0);
+}
---
+++ app/lib/lcd1602.h
@@ -0,0 +1,52 @@
+#include <stdint.h>
+#include "main.h"
+
+#define PIN_RS (1 << 0)
+#define PIN_EN (1 << 2)
+#define BACKLIGHT (1 << 3)
+#define OSS 0
+
+uint16_t lcd1604_addr = 0x27 << (uint16_t)1;
+
+/*
+ * @brief function for init 1602 lcd
+ *        display
+ * @param void
+ * @return void
+ */
+void lcd1602_init(void);
+
+/*
+ * @brief function for transmitting
+ *        a data via I2C to 1602
+ * @param uint8_t data - transmitting data
+ * @return int         - result success flag
+ *
+ */
+void lcd1602_transmit_data(uint8_t data);
+
+/*
+ * @brief function for transmitting
+ *        a command via I2C to 1602
+ * @param uint8_t cmd - transmitting command
+ * @return int        - result success flag
+ */
+void lcd1602_transmit_command(uint8_t cmd);
+
+/*
+ * @brief general purpose function for 
+ *        transmitting any shit what
+ *        you wanna transmit
+ * @param uint8_t data  - transmitting data
+ * @param uint8_t flags - flags of transmitting
+ * @return void
+ */
+HAL_StatusTypeDef lcd1602_transmit(uint8_t data, uint8_t flags);
+
+/*
+ * @brief function of transmitting a
+ *        string to show it on 1602
+ * @param char* sending string
+ * @return void
+ */
+void lcd1602_send_string(char *str);
